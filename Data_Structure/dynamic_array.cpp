985e79993a3b5bee88c9b21bd59f461ef9431b31
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-07-28 23:59:18 +0600

    input format ready

--- Data_Structure/dynamic_array.cpp
+++ Data_Structure/dynamic_array.cpp
@@ -6,7 +6,13 @@ using namespace std;
 
 int main()
 {
-
+    int n,q;
+    cin >> n >>q;
+    string queries[q];
+    for(int i=0;i<q;i++)
+    {
+        cin>>queries[i];
+    }
     return 0;
 }
 
