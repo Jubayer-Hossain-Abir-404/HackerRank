75fbdc6a30fa482bf396ca60a1c3bc5645be4195
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-07-29 01:54:14 +0600

    dynamic array code complete

--- Data_Structure/dynamic_array.cpp
+++ Data_Structure/dynamic_array.cpp
@@ -2,7 +2,36 @@
 
 using namespace std;
 
+int getIdxValue(char ch, int lastAnswer, int n)
+{
+    int x = int(ch - '0');
+    return ((x ^ lastAnswer) % n);
+}
 
+void dynamicArray(int n, string queries[], int q)
+{
+    vector<vector<int>> arr(n);
+    int lastAnswer = 0, c=0;
+    int ans[q];
+
+    for(int i=0;i<q;i++)
+    {
+        int idx = getIdxValue(queries[i][2], lastAnswer, n);
+        if(queries[i][0]=='1')
+        {
+            arr[idx].push_back(int(queries[i][4] - '0'));
+        }
+        else
+        {
+            lastAnswer = arr[idx][int(queries[i][4] - '0') % arr[idx].size()];
+            ans[c++] = lastAnswer;
+        }
+    }
+    for(int i=0;i<c;i++)
+    {
+        cout<<ans[i]<<endl;
+    }
+}
 
 int main()
 {
@@ -11,8 +40,13 @@ int main()
     string queries[q];
     for(int i=0;i<q;i++)
     {
-        cin>>queries[i];
+        getline(cin, queries[i]);
+        while (queries[i].length() == 0)
+        {
+            getline(cin, queries[i]);
+        }
     }
+    dynamicArray(n, queries, sizeof(queries)/sizeof(queries[0]));
     return 0;
 }
 
